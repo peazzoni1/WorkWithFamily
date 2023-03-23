8a2b252502b83ecfa1be93387f6f41e601865d2d
Aaron Peazzoni <peazzoni@gmail.com>
2023-03-22 22:59:43 -0400

    assessment form style and animation

--- src/app/assessment/assessment.component.ts
+++ src/app/assessment/assessment.component.ts
@@ -1,14 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import {
+  animate,
+  animateChild,
+  animation,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-assessment',
   templateUrl: './assessment.component.html',
-  styleUrls: ['./assessment.component.css']
+  styleUrls: ['./assessment.component.css'],
+  animations: [
+    trigger('ngIfAnimation', [
+      transition(':enter, :leave', [
+        query('@*', animateChild())
+      ])
+    ]),
+    trigger('nextQuestion', [
+      transition(
+        'void => *',
+        animation([
+          style({
+            transform: 'translate(200px,0)',
+          }),
+          animate(
+            '0.5s cubic-bezier(0.59, 0.32, 0.38, 1.13)',
+            style({
+              transform: 'translate(0)',
+            })
+          ),
+        ])
+      ),
+      transition(
+        '* => void',
+        animation([
+          style({ transform: 'translate(0)' }),
+          animate(
+            '0.5s cubic-bezier(0.59, 0.32, 0.38, 1.13)',
+            style({
+              transform: 'translate(-200px,0)',
+            })
+          ),
+        ])
+      ),
+    ]),
+  ],
 })
 export class AssessmentComponent implements OnInit {
   assessment: FormGroup | any;
-
+  loadQuestion:boolean = true;
   questions = [
     {type: "name", description : "What is your name ?", isHidden:false},
     {type: "email", description : "What is your email ?", isHidden:true},
@@ -25,5 +69,11 @@ export class AssessmentComponent implements OnInit {
 
   onSubmit() {
 
+  }
+
+  nextQuestion(item: any) {
+    const currIndex = this.questions.indexOf(item);
+    this.questions[currIndex].isHidden = true;
+    this.questions[currIndex + 1].isHidden = false;
   }
 }
